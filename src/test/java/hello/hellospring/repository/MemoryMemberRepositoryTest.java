475e5ff483e2644b3dbb7da114c20a0faf80577d
geunhokinn <gghh1220@gmail.com>
2024-04-05 20:19:40 +0900

    test: 회원 리포지토리 테스트 추가

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach // 콜백 메서드
+    public void afterEach() {
+        repository.clearStore();
+    } // test 가 끝날 때마다 저장소를 비움.
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        // new 로 만든 거랑, 메모리에 저장된 거랑 같으면 참
+//        System.out.println("result = "+ (member==result));
+//        Assertions.assertEquals(member, result);
+        // 기대한 값, 실제 값이 같아야함
+        assertThat(member).isEqualTo(result);
+        // 요즘은 이렇게 더 편하게 한다.
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
